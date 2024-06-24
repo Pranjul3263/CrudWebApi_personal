f9dc69e198a417440b97d4907723db8ab8090bc0
Pranjul3263 <120020163+Pranjul3263@users.noreply.github.com>
2024-06-24 11:18:07 +0530

    addApi

---
+++ webApi/Data/AppDbConst.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webApi.Model.Entity;
+
+namespace webApi.Data
+{
+    public class AppDbConst : DbContext
+    {
+        public AppDbConst(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
